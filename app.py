ffcf9b93ea237dcfe3c6cc5931e66d306ca8097d
Sunandana Sahoo <sunandana28@gmail.com>
2026-06-07 23:52:13 +0200

    changes

--- app.py
+++ app.py
@@ -60,7 +60,8 @@ def add():
         gender = request.form["gender"]
         relation = request.form["relation"]
         account = request.form["account"]
-        share_percentage = float(request.form["share_percentage"])
+        share_percentage = request.form["share_percentage"].replace("%", "")
+        share_percentage = float(share_percentage)
         nominee_type = request.form["nominee_type"]
 
         # ✅ validation
