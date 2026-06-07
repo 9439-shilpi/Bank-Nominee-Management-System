b9c29bb008641f3ccbd5dc1fbd0221cc120c3624
Sunandana Sahoo <sunandana28@gmail.com>
2026-06-07 21:32:58 +0200

    db.py

--- db.py
+++ db.py
@@ -1,9 +1,11 @@
+import os
 import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host="127.0.0.1",
-        user="root",
-        password="NewPassword@123",
-        database="bank_nominee_db"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=int(os.getenv("DB_PORT", 3306))
     )
